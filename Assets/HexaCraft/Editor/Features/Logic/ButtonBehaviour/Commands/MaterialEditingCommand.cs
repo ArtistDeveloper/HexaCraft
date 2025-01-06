cfd96f40cb268245a00072705a31f372f28a22e9
JunsuPark <somadubel@gmail.com>
2025-01-07 03:57:27 +0900

    [Refactor] Receiver와 Command 분리

--- Assets/HexaCraft/Editor/Features/Logic/ButtonBehaviour/Commands/MaterialEditingCommand.cs
+++ Assets/HexaCraft/Editor/Features/Logic/ButtonBehaviour/Commands/MaterialEditingCommand.cs
@@ -1,6 +1,33 @@
+using UnityEditor;
 using UnityEngine;
 
-public class MaterialEditingCommand
+namespace HexaCraft
 {
-    
+    public class MaterialEditingCommand : ICommand
+    {
+        public MaterialEditingReceiver _receiver;
+
+        public MaterialEditingCommand(MaterialEditingReceiver receiver)
+        {
+            _receiver = receiver;
+        }
+
+        public void Execute(SceneView sceneView, Event evt)
+        {
+            if (evt.button == 1)
+                return;
+
+            if (evt.type == EventType.MouseDown)
+            {
+                evt.Use();
+                _receiver.ApplyMaterialChange(evt);
+            }
+            else if (evt.type == EventType.MouseDrag)
+            {
+                evt.Use();
+                _receiver.ApplyMaterialChange(evt);
+            }
+        }
+    }
 }
+
--- Assets/HexaCraft/Editor/Features/Logic/ButtonBehaviour/Receiver/MaterialEditingReceiver.cs
+++ Assets/HexaCraft/Editor/Features/Logic/ButtonBehaviour/Receiver/MaterialEditingReceiver.cs
@@ -1,6 +1,36 @@
+using UnityEditor;
 using UnityEngine;
 
-public class MaterialEditingReceiver
+namespace HexaCraft
 {
-    
+    public class MaterialEditingReceiver
+    {
+        // TODO: _presenter를 직접 참조하는 것이 아닌, 이벤트 형식으로 변경할 필요 존재.
+        // private readonly HCPresenter _presenter;
+
+        private Vector2 _mousePosition;
+
+        private Ray _ray;
+
+        private RaycastHit _hit;
+
+
+        public void ApplyMaterialChange(Event evt)
+        {
+            _mousePosition = evt.mousePosition;
+            _ray = HandleUtility.GUIPointToWorldRay(_mousePosition);
+            if (Physics.Raycast(_ray, out _hit))
+            {
+                GameObject selectedObject = _hit.collider.gameObject;
+                Material[] materials = new Material[selectedObject.GetComponent<Renderer>().sharedMaterials.GetLength(0)];
+
+                for (int i = 0; i < materials.GetLength(0); i++)
+                {
+                    // materials[i] = _presenter.GetSelectionMaterial();
+                }
+
+                selectedObject.GetComponent<Renderer>().sharedMaterials = materials;
+            }
+        }
+    }
 }
