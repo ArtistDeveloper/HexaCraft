de92cafcb078a0ec8f0a09598436ce381fef72c9
JunsuPark <somadubel@gmail.com>
2025-03-21 16:07:31 +0900

    [Fix] HCMainView대신 HCGenerationEditor적용

--- Assets/HexaCraft/Editor/Features/UI/HCMainView.cs
+++ Assets/HexaCraft/Editor/Features/UI/HCMainView.cs
@@ -8,7 +8,7 @@ using UnityEngine.UI;
 
 namespace HexaCraft
 {
-    [CustomEditor(typeof(HCGeneration))]
+    // [CustomEditor(typeof(HCGeneration))]
     public class HCMainView : Editor, IHCMainView
     {
         private GUIStyle _mainHeaderStyle;
